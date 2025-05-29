3ef19440cb7d44335087d82e4283d33dd6edc04c
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-29 11:48:46 +0300

    fix: define the __str__ method for the Book model inside  the model class

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -6,5 +6,5 @@ class Book(models.Model):
     author = models.CharField(max_length= 100)
     publication_year = models.DateField()
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
