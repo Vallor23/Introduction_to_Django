f170d180ef035c6a907ad402b78eb5c4b4111725
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-31 06:00:14 +0300

    Add views

--- LibraryProject/bookshelf/views.py
+++ LibraryProject/bookshelf/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BookSerializer
+from .models import Book
 
 # Create your views here.
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
