d99b6565c385aab6542db284a0561a7338ab3c79
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-12 00:00:41 +0300

    Create models

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length= 200)
     author = models.CharField(max_length= 100)
     publication_year = models.DateField()
-    created_at = models.DateTimeField(auto_now_add= True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+    class Meta:
+        ordering = ['-publication_year']
     def __str__(self):
         return self.title
