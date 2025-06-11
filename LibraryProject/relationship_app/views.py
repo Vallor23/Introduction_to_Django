53518881b3f91848f6d03aec8459e8e3afb1d886
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-12 00:43:56 +0300

    Create view to list all book instances from the database

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book, Library
 
 # Create your views here.
+def book_list(request):
+     """Retrieves all books and renders a template displaying the list."""
+     books = Book.objects.all()  # Fetch all book instances from the database
+     return render(request, context={'book_list': books})  # Rebder context dictionary with book list
