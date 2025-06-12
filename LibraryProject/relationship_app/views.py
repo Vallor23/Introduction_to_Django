c21b213141e54eff04a76a01db67f6f2ff55a934
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-12 10:47:24 +0300

    Create register view using UserCreationForm from django

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book, Library
 from django.views.generic import DetailView
+from django.views import View
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def booklist_view(request):
@@ -13,3 +15,16 @@ class LibraryDetailView(DetailView):
      model =  Library
      template_name = "relationship_app/library_detail.html"
      context_object_name = "library"
+
+# Autentication Views
+class RegisterView(View):
+    def get(self, request):
+     form = UserCreationForm()
+     return render(request,'relationship_app/register.html',{'form':form})
+    
+    def post(self, request):
+     form= UserCreationForm(request.POST)
+     if form.is_valid():
+          form.save()  #creates and saves te new user
+          return redirect('login')  # Redirect to login page after successful registartion
+     return render(request,'relationship_app/register.html',{'form':form})
