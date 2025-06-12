c2e2e3a05cb133b583ead0b8d4c6b812d4313425
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-12 14:48:44 +0300

    Add default role to role attr

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -42,8 +42,8 @@ class UserProfile(models.Model):
         ('librarian','Librarian'),
         ('member','Member')
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', default='member')
-    role = models.CharField(choices=ROLE_CHOICES, max_length=10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(choices=ROLE_CHOICES, max_length=10, default='member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
