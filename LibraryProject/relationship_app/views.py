38d122eab4a7748563b6dc47dfac4e24518cff53
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-12 15:47:13 +0300

    Implement Role-Based Access Control

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import Book, Library
+from .models import Book, Library, UserProfile
 from django.views.generic import DetailView
 from django.views import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 def booklist_view(request):
@@ -28,3 +29,28 @@ class RegisterView(View):
           form.save()  #creates and saves te new user
           return redirect('login')  # Redirect to login page after successful registartion
      return render(request,'relationship_app/register.html',{'form':form})
+
+
+# check functions for the user’s role before granting access to each view
+def is_admin(user):
+     return hasattr(user, 'profile') and user.profile.role == 'admin'
+
+def is_librarian(user):
+     return hasattr(user, 'profile') and user.profile.role == 'librarian'
+
+def is_member(user):
+     return hasattr(user, 'profile') and user.profile.role == 'member'
+
+
+# Role-Based Views to manage content access based on user roles
+@user_passes_test(is_admin)
+def admin_view(request):
+     return render(request,'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian) 
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+     return render(request,'relationship_app/member_view.html')
