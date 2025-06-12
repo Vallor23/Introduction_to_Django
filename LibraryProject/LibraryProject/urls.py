c3cba8a39a3af7b41faf6093324f0730552ed44b
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-12 16:36:55 +0300

    Add custom permission urls

--- LibraryProject/LibraryProject/urls.py
+++ LibraryProject/LibraryProject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from bookshelf.views import BookListCreateAPIView
-from relationship_app.views import LibraryDetailView, booklist_view, RegisterView, admin_view, librarian_view, member_view
+from relationship_app.views import LibraryDetailView, booklist_view, RegisterView, admin_view, librarian_view, member_view,add_book, delete_book, edit_book
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +33,8 @@ urlpatterns = [
     path('admin-only/', admin_view, name="admin_view"),
     path('librarian-only/', librarian_view, name="librarian_view"),
     path('member-only/', member_view, name="member_view"),
+    # custom permission urls
+    path('api/books/add_book/', add_book, name="add_book"),
+    path('api/books/delete_book/<int:pk>/', delete_book, name="delete_book"),
+    path('api/books/edit_book/<int:pk>/', edit_book, name="edit_book"),
 ]
