508efe7b7e9574242b23e24326111cbe6a4ea1e0
Vallor23 <enosnyabinda2017@gmail.com>
2025-06-12 17:37:52 +0300

    Add custom permissions

--- LibraryProject/relationship_app/models.py
+++ LibraryProject/relationship_app/models.py
@@ -18,7 +18,11 @@ class Book(models.Model):
     publication_year = models.DateField(default=timezone.now)
     class Meta:
         ordering = ['-publication_year']
-    
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
     def __str__(self):
         return f"{self.title} by {self.author}" 
     
